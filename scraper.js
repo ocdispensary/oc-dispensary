abed563bbf1134f429713fd0b3282cd9b012fd92
ajdorfman93 <dorfmanart@gmail.com>
2025-05-11 14:05:12 -0400

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -94,7 +94,7 @@ if (isCI) {
           img  : imgEl?.src || "",
           // --- remove a trailing ellipsis (“…”) if present ---
           brand: brandEl
-            ? brandEl.textContent.replace(/\u2026$/, "").trim()
+            ? brandEl.textContent.replace(/\u002E$/, "").trim()
             : "",
           link : ocLink,
           price: priceEl?.textContent.trim() || ""
